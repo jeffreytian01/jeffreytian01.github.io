3a03a8c379da38c2c1426df4289daef21d58dd48
Jeffrey Tian <jefftian@umich.edu>
2021-11-14 05:22:19 -0500

    background changes

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var count1 = 0;
+var count2 = 0;
+var txt = "I am Jeffrey Tian.";
+var txt2 = "Creator. Entrepreneur. Innovator."
+var speed = 60;
+var first = true;
+
+window.onload = function typewriter() {
+    if (count1 < txt.length && first) {
+      document.getElementById("header-intro-1").innerHTML += txt.charAt(count1);
+      count1++;
+      setTimeout(typewriter, speed);
+    }
+    else {
+        first = false;
+        if (count2 < txt2.length) {
+            document.getElementById("header-intro-2").innerHTML += txt2.charAt(count2);
+            count2++;
+            setTimeout(typewriter, speed);
+          }
+    }
+  }
